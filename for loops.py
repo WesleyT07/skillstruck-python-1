f3c44f8184f0f255e8743801f45d35edf6f43e1c
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-08-30 12:34:21 -0700

    exported from Skill Struck

---
+++ for loops.py
@@ -0,0 +1,15 @@
+#This is the checkeckpoint
+#animals = ['dog','cat',"cow",'frog']
+#for x in animals:
+#    print(x + " is the spikiest animal ever!")
+#This is challenge 1
+#var1 = int(input("Enter number 1"))
+#var2 = int(input("enter number 2"))
+#for x in range (var1, var2 + 1):
+#    print(x)
+#This is challenge 2
+#var = int(input("Enter number"))
+#result = 1
+#for x in range(1, var + 1):
+#    result = x * result
+#print(result)
