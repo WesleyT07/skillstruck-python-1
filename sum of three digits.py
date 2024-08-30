86db484ad4f1c74540fc623272684a5e963cf639
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-08-30 12:34:21 -0700

    exported from Skill Struck

---
+++ sum of three digits.py
@@ -0,0 +1,7 @@
+thr = input("please enter your three numbers")
+first = int(thr[0])
+second = int(thr[1])
+third = int(thr[2])
+total = first+second+third
+string1 = 'The sum of those digits is {}'
+print(string1.format(total))
